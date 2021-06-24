5c268e8d7e3db8141bc1afcb7e1a7a4d4aa2ebed
PIKAniki <85255630+PIKAniki@users.noreply.github.com>
2021-06-24 22:56:25 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -10,8 +10,8 @@
             modal.style.display = 'block';
             $('.modal-slider').slick({
                 centerMode: true,
-                centerPadding: '60px',
                 slidesToShow: 3,
+                speed: 600,
                 arrows: true,
                 variableWidth: true,
                 infinite: true,
@@ -19,6 +19,7 @@
                 prevArrow: '<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18"fill="none" xmlns="http://www.w3.org/2000/svg"><path d = "M9.21839 1L1 9L9.21839 17"/></svg></button>',
                 nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d = "M0.78161 17L9 9L0.78161 1"/></svg></button> '
             });
+
             document.body.style.overflow = 'hidden';
             document.body.style.marginRight = '17px';
 
@@ -61,6 +62,7 @@
 
 
 })();
+
 // $(function() {
 //     $('.modal').show();
 //     $('.modal-slider').slick({
