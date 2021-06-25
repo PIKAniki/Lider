3d266a4596c22d72fbddf1cf8b4c0e5035e9ba72
PIKAniki <85255630+PIKAniki@users.noreply.github.com>
2021-06-26 00:17:21 +0300

    Add files via upload

--- js/magnific-popup.js
+++ js/magnific-popup.js
@@ -1,14 +1,5 @@
 (function() {
-    // var groups = {};
-    // $('.modal-body__images-link').each(function() {
-    //     var id = parseInt($(this).attr('data-group'), 10);
 
-    //     if (!groups[id]) {
-    //         groups[id] = [];
-    //     }
-
-    //     groups[id].push(this);
-    // });
 
     $('.modal-body__images').each(function() {
         $(this).magnificPopup({
@@ -20,7 +11,6 @@
             gallery: {
                 enabled: true
             },
-
             zoom: {
                 enabled: true,
 
@@ -33,26 +23,9 @@
             }
 
         });
+
+
     });
 
-    // $('.modal-body__images-2').magnificPopup({
-    //     delegate: 'a',
-    //     type: 'image',
-    //     mainClass: 'mfp-with-zoom',
-    //     gallery: {
-    //         enabled: true
-    //     },
 
-    //     zoom: {
-    //         enabled: true,
-
-    //         duration: 300,
-    //         easing: 'ease-in-out',
-    //         opener: function(openerElement) {
-
-    //             return openerElement.is('img') ? openerElement : openerElement.find('img');
-    //         }
-    //     }
-
-    // });
 })();
