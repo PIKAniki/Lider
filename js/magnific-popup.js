dc33845c00c866e714a5bc771be7061b57dd230f
PIKAniki <85255630+PIKAniki@users.noreply.github.com>
2021-06-25 22:37:24 +0300

    Add files via upload

--- js/magnific-popup.js
+++ js/magnific-popup.js
@@ -12,9 +12,11 @@
 
     $('.modal-body__images').each(function() {
         $(this).magnificPopup({
-            delegate: 'a',
+            delegate: 'a:not(.slick-cloned)',
             type: 'image',
             mainClass: 'mfp-with-zoom',
+            fixedContentPos: false,
+            fixedBgPos: true,
             gallery: {
                 enabled: true
             },
